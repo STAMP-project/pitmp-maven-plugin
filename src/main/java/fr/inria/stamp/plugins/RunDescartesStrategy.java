aca7dbe605d201fde50004b4bc23d4a58dbf22f1
Caroline Landry <caroline.landry@inria.fr>
2017-10-20 16:54:25 +0200

    keeping all project and modules data

--- src/main/java/fr/inria/stamp/plugins/RunDescartesStrategy.java
+++ src/main/java/fr/inria/stamp/plugins/RunDescartesStrategy.java
@@ -3,7 +3,9 @@ package fr.inria.stamp.plugins;
 // **********************************************************************
 import java.io.File;
 import java.util.Map;
+import java.util.ArrayList;
 
+import org.apache.maven.project.MavenProject;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.pitest.mutationtest.config.PluginServices;
 import org.pitest.mutationtest.config.ReportOptions;
@@ -19,6 +21,12 @@ public class RunDescartesStrategy implements GoalStrategy
    // **********************************************************************
    // public
    // **********************************************************************
+   // ******** attributes
+   public static MavenProject rootProject = null;
+   public static ArrayList<MavenProject> projectList = new ArrayList<MavenProject>();
+   public static ArrayList<ReportOptions> projectData = new ArrayList<ReportOptions>();
+   public static int currentProject = -1;
+
    // ******** methods
    @Override
    public CombinedStatistics execute(File baseDir, ReportOptions data,
@@ -27,7 +35,11 @@ public class RunDescartesStrategy implements GoalStrategy
    {
       System.out.println("################ RunDescartesStrategy: " + baseDir);
       System.out.println("# baseDir: " + baseDir);
-      System.out.println("########################################");
+      System.out.println("# rootProject: " + rootProject.getArtifactId());
+      System.out.println("# plugins: " + plugins);
+      System.out.println("# environmentVariables: " + environmentVariables);
+      System.out.println("################");
+      printInfo();
 
       EntryPoint e = new EntryPoint();
       AnalysisResult result = e.execute(baseDir, data, plugins,
@@ -38,5 +50,34 @@ public class RunDescartesStrategy implements GoalStrategy
       }
 
       return result.getStatistics().value();
+   }
+
+   // **********************************************************************
+   public void printInfo()
+   {
+      System.out.println("################################ Info");
+      System.out.println("# projects count: " + projectList.size());
+      System.out.println("# current project: " + currentProject);
+      for (int i = 0; i < projectList.size(); i++)
+      {
+         printProjectInfo(projectList.get(i), projectData.get(i));
+      }
+      System.out.println("################################");
+
+   }
+
+   // **********************************************************************
+   public void printProjectInfo(MavenProject aProject, ReportOptions projectData)
+   {
+      System.out.println("################ project: " + aProject.getArtifactId());
+      System.out.println("#");
+      System.out.println("# targetTests: " + projectData.getTargetTests());
+      System.out.println("# targetClasses: " + projectData.getTargetClasses());
+      System.out.println("# codePaths: " + projectData.getCodePaths());
+      System.out.println("# sourceDirs: " + projectData.getSourceDirs());
+      System.out.println("# classPathElements: " + projectData.getClassPathElements());
+      System.out.println("# mutationEngine: " + projectData.getMutationEngine());
+      System.out.println("################");
+
    }
 }
